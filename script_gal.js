2f1b4726072baed84889ea6c4b912cb8ffb2c7dc
daemon153 <mwirth77@gmail.com>
2024-11-08 18:30:44 +0100

    Update script_gal.js

--- script_gal.js
+++ script_gal.js
@@ -1,8 +1,36 @@
 // Array mit Bild-URLs
 const imageUrls = [
-    "https://drive.google.com/uc?id=19qwqNSMPt5NdY4iB5Ebv0mVNs10CZRqZ",
-    "https://drive.google.com/uc?id=19pGUm3zY3Gakar6rtrY0WW_E86yQn1pQ",
-    "https://drive.google.com/uc?id=18rAihBXaCp1ysgi7zUNuJRzxfO0NkTkT",
+    const imageUrls = [
+    "pix/Resize_20241108_181342_2357.jpg",
+    "pix/Resize_20241108_181342_2907.jpg",
+    "pix/Resize_20241108_181343_3317.jpg",
+    "pix/Resize_20241108_181344_4120.jpg",
+    "pix/Resize_20241108_181344_4719.jpg",
+    "pix/Resize_20241108_181345_5167.jpg",
+    "pix/Resize_20241108_181347_7213.jpg",
+    "pix/Resize_20241108_181348_8159.jpg",
+    "pix/Resize_20241108_181349_9639.jpg",
+    "pix/Resize_20241108_181350_0198.jpg",
+    "pix/Resize_20241108_181351_1007.jpg",
+    "pix/Resize_20241108_181351_1419.jpg",
+    "pix/Resize_20241108_181351_1824.jpg",
+    "pix/Resize_20241108_181352_2264.jpg",
+    "pix/Resize_20241108_181352_2607.jpg",
+    "pix/Resize_20241108_181353_3025.jpg",
+    "pix/Resize_20241108_181353_3651.jpg",
+    "pix/Resize_20241108_181353_3985.jpg",
+    "pix/Resize_20241108_181354_4290.jpg",
+    "pix/Resize_20241108_181354_4811.jpg",
+    "pix/Resize_20241108_181354_4998.jpg",
+    "pix/Resize_20241108_181355_5719.jpg",
+    "pix/Resize_20241108_181356_6125.jpg",
+    "pix/Resize_20241108_181356_6335.jpg",
+    "pix/Resize_20241108_181356_6982.jpg",
+    "pix/Resize_20241108_181357_7394.jpg",
+    "pix/Resize_20241108_181357_7763.jpg",
+    "pix/Resize_20241108_181358_8238.jpg",
+    "pix/Resize_20241108_181358_8527.jpg"
+];
     // Füge hier weitere Bild-URLs hinzu
 ];
 
